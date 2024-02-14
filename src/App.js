4e70107b0ac29a2c424ab0e0a119f710a0d40e52
cw-deepanshu <deepanshu.d@carwale.com>
2024-02-14 16:37:15 +0530

    frontend

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+
+import "./index.css";
+import FileUpload from "./FileUpload";
+function App() {
+    return (
+        <div className="main-container">
+            <h1>Hacktivate 6.0</h1>
+            <FileUpload />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/FileUpload.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import axios from "axios";
+const FileUpload = () => {
+    const [file, setFile] = useState(null);
+
+    const handleFileChange = (event) => {
+        setFile(event.target.files[0]);
+        //console.log(event);
+    };
+
+    const handleSubmit = async (e) => {
+        const data = new FormData();
+
+        const config = {
+            headers: {
+                "Content-Type": "multipart/form-data"
+            }
+        };
+
+        data.append("file", file);
+        console.log(data);
+        const response = await axios.post(
+            "http://localhost:5000/upload",
+            data,
+            config
+        );
+        console.log(response);
+    };
+    return (
+        <div className="file-contaner">
+            <div class="container">
+                <div class="card">
+                    <h3>Upload Files</h3>
+                    <div class="drop_box">
+                        <header>
+                            <h4>Select File here</h4>
+                        </header>
+                        <p>Files Supported: .MP3</p>
+                        <input
+                            className="btn"
+                            type="file"
+                            accept=".mp3"
+                            multiple
+                            onChange={handleFileChange}
+                        />
+                        <button className="submit" onClick={handleSubmit}>
+                            Submit
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FileUpload;
